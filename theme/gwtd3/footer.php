60e649a9b2d65a78094b992a83294f1cd4b37b3e
Xenos Konstantinos <mr.xkon@gmail.com>
2017-07-25 02:23:37 +0300

    gwtd3 theme v1.0.0
    general landing page setup
    general functions
    folder structure
    
    .gitignore updates for Windows, PhpStorm, WordPress

---
+++ theme/gwtd3/footer.php
@@ -0,0 +1,16 @@
+<div class="section footer">
+	<div class="container">
+		<div class="row">
+			<div class="six columns">
+				<h1 class="value-multiplier">Stay tuned!</h1>
+			</div>
+			<div class="six columns text-right">
+				<h3 class="value-multiplier">icon icon icon</h3>
+			</div>
+		</div>
+	</div>
+</div>
+
+<?php wp_footer(); ?>
+</body>
+</html>
---
+++ theme/gwtd3/template-parts/hero.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Displays hero banner
+ */
+?>
+<div class="section hero">
+	<div class="container">
+		<div class="row">
+			<div class="twelve columns text-center">
+				<h1 class="hero-heading">30|9|17</h1>
+			</div>
+		</div>
+	</div>
+</div><!-- .custom-hero -->
