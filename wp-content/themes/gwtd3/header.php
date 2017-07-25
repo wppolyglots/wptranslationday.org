ad39ae88850328537ea95c79abe8b33ff4980bff
mrxkon <mr.xkon@gmail.com>
2017-07-25 12:54:11 +0300

    first commit
    first commit

---
+++ wp-content/themes/gwtd3/header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
---
+++ wp-content/themes/gwtd3/index.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Landing page
+ */
+
+get_header();
+
+get_template_part( 'template-parts/hero', 'hero' );
+
+//get_template_part( 'template-parts/menu', 'menu' );
+?>
+	<div class="section lp-what-it-is">
+		<div class="container">
+			<div class="row">
+				<div class="twelve columns">
+					<?php
+					$page = get_page_by_title( 'What is GWTD3?' );
+					$title = $page->post_title;
+					echo '<h1 class="value-multiplier">' . $title . '</h1>';
+					?>
+				</div>
+			</div>
+			<div class="row">
+				<div class="eleven columns offset-by-one">
+					<?php echo $page->post_content; ?>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="section lp-where-it-is">
+		<div class="container">
+			<div class="row">
+				<div class="twelve columns">
+					<?php
+					$page = get_page_by_title( 'Where is GWTD3?' );
+					$title = $page->post_title;
+					echo '<h1 class="value-multiplier">' . $title . '</h1>';
+					?>
+				</div>
+			</div>
+			<div class="row">
+				<div class="eleven columns offset-by-one">
+					<?php echo $page->post_content; ?>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="section lp-how-to-get-involved">
+		<div class="container">
+			<div class="row">
+				<div class="twelve columns">
+					<?php
+					$page = get_page_by_title( 'Cool! How do I get involved?' );
+					$title = $page->post_title;
+					echo '<h1 class="value-multiplier">' . $title . '</h1>';
+					?>
+				</div>
+			</div>
+			<div class="row">
+				<div class="eleven columns offset-by-one">
+					<?php echo $page->post_content; ?>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+<?php
+get_footer();
