b18590e142a8b597d84c521b41360e017e2208a8
dinhtungdu <dinhtungdu@gmail.com>
2017-07-28 00:59:36 +0700

    Add countdown timer

--- wp-content/themes/gwtd3/landing-page.php
+++ wp-content/themes/gwtd3/landing-page.php
@@ -44,6 +44,22 @@
 		</div>
 	</div>
 
+	<div id="when" class="section lp-when-it-is">
+		<div class="container">
+			<div class="row">
+				<div class="twelve columns">
+					<h2><?php echo 'When is GWTD3?'; ?></h2>
+				</div>
+			</div>
+			<div class="row">
+				<div class="eleven columns offset-by-one">
+					<p><strong>Global WordPress Translation Day</strong> will be from <strong>00.00 to 24.00 UTC on September 30, 2017.</strong> To help you get ready, here's the countdown to kick off:</p>
+					<div id="countdown"></div>
+				</div>
+			</div>
+		</div>
+	</div>
+
 	<div id="how" class="section lp-how-to-get-involved">
 		<div class="container">
 			<div class="row">
