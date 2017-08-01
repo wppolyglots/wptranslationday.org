608204467dfc1b98921bafb948e0a4f6858944a7
ePascalC <pascal.casier@gmail.com>
2017-08-01 11:15:18 +0200

    Adding v0.1 Local Events plugin

---
+++ wp-content/plugins/gwtd-local-events/gwtd-local-events.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Plugin Name: GWTD Local Events
+Plugin URI: https://wptranslationday.org
+Description: This plugin will deal with local events.
+Version: 0.1
+Author: Pascal CASIER
+Author URI: http://www.facebook.com/pascal.casier
+License: GPL2
+*/
+
+include( dirname(__FILE__) . '/inc/setup.php');
+
+?>
