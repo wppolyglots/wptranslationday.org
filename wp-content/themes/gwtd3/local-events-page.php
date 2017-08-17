ddd8b97fd88fb8330d92f37883354d242e201da5
ePascalC <pascal.casier@gmail.com>
2017-08-17 16:25:34 +0200

    Create template for local events
    This template is applied to the page 'Local Events'. The page title and page content are used. After that all local events in the database are displayed ordered by Region/Country/City

---
+++ wp-content/themes/gwtd3/local-events-page.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Template Name: Local Events
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="bg-color-blue--neutral-light text-color-blue--darker section">
+		<div class="container">
+			<div class="row">
+				<div class="twelve columns">
+					<header class="page-header">
+						<?php
+							$page = get_post( 418 );
+							echo '<h2>' . $page->post_title . '</h2>';
+							echo $page->post_content;
+							echo '<br><br>';
+						?>
+					</header><!-- .page-header -->
+		
+					<?php
+					$query = new WP_Query( array('post_type' => 'local-event', 'posts_per_page' => -1, 'meta_key' => 'full_place', 'orderby' => 'meta_value', 'order' => 'ASC' ) );
+					$previous_continent = 'Empty';
+					while ( $query->have_posts() ) : $query->the_post();
+						$continent = get_post_meta($post->ID, 'continent', true);
+						if ( $previous_continent != $continent ) {
+							if ( $previous_continent != 'Empty' ) {
+								echo '<br>';
+							}
+							$previous_continent = $continent;
+							echo '<h2>' . $continent . '</h2>';
+						} ?>
+						<div class="entry-content">
+						<?php echo '<b>' . get_post_meta( $post->ID, 'country', true ) . ' / ' . get_post_meta( $post->ID, 'city', true ) . '</b> for locale(s) <b>' . get_post_meta( $post->ID, 'locale', true ) . '</b>.';
+						$utc_start = get_post_meta( $post->ID, 'utc_start', true );
+						if ($utc_start) {
+							echo ' Starting at ' . $utc_start . ' UTC';
+						}
+						echo '<br>';
+						$w_org = get_post_meta( $post->ID, 'organizer_w_org', true );
+						echo 'Organizer: <a href="https://profiles.wordpress.org/' . $w_org . '">' . $w_org . '</a>';
+						$url = get_post_meta( $post->ID, 'announcement_url' , true );
+						if ($url) {
+							echo ', <a href="' . $url . '">announcement url</a>';
+						}
+						echo '<br>';
+						 ?>
+						</div>
+					<?php
+					endwhile;
+					wp_reset_postdata();
+					?>
+				</div>
+			</div>
+		</div>
+	</div>
+
+<?php
+get_sidebar();
+get_footer();
