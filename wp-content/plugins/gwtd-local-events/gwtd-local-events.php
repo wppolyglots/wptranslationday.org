af56ea1a7099350109680935392cb42154e46fc6
Xenos (xkon) Konstantinos <mr.xkon@gmail.com>
2017-08-29 20:06:07 +0300

    local events plugin css fix
    add responsive css
    add 100% width to input fields

--- wp-content/plugins/gwtd-local-events/gwtd-local-events.php
+++ wp-content/plugins/gwtd-local-events/gwtd-local-events.php
@@ -11,4 +11,10 @@ License: GPL2
 
 include( dirname(__FILE__) . '/inc/setup.php');
 
+function gwtd_local_events_load_scripts()
+{
+	wp_enqueue_style( 'gwtd-local-events', plugin_dir_url( __FILE__ ) . 'inc/style.css', array(), '0.3' );
+}
+add_action( 'admin_enqueue_scripts', 'gwtd_local_events_load_scripts' );
+
 ?>
