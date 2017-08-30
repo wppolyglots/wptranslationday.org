6e9b7a1788323f036436fa265d6dec6b1473f53a
ePascalC <pascal.casier@gmail.com>
2017-08-30 10:18:23 +0200

    Create gwtd-the-team.php

---
+++ wp-content/plugins/gwtd-the-team/gwtd-the-team.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Plugin Name: GWTD The Team
+Plugin URI: https://wptranslationday.org
+Description: This plugin will deal with the organizers and volunteers.
+Version: 0.1
+Author: Pascal CASIER
+Author URI: http://www.facebook.com/pascal.casier
+License: GPL2
+*/
+
+include( dirname(__FILE__) . '/inc/setup.php');
+
+?>
