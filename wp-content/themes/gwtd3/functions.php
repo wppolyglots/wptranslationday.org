f109355258d04ddcfe278485f719f47f5f233fc9
Xenos (xkon) Konstantinos <mr.xkon@gmail.com>
2017-08-31 11:49:12 +0300

    masonry js
    masonry js

--- wp-content/themes/gwtd3/functions.php
+++ wp-content/themes/gwtd3/functions.php
@@ -169,6 +169,12 @@ function gwtd3_scripts() {
         wp_enqueue_script( 'gwtd-map' );
 	}
 
+	if ( is_page( 'social-mentions' ) ) {
+		wp_enqueue_script( 'gwtd3-masonry', get_template_directory_uri() . '/js/masonry.pkgd.min.js', array( 'jquery' ), '20170725', true );
+		wp_enqueue_script( 'gwtd3-imagesloaded', get_template_directory_uri() . '/js/imagesLoaded.pkgd.min.js', array( 'jquery' ), '20170725', true );
+
+	}
+
 	wp_enqueue_script( 'gwtd3-custom-scripts', get_template_directory_uri() . '/js/scripts.js', array( 'jquery' ), '20170725', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
