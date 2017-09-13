ba0c7da824ec953905551952b6de771b842fdf51
Xenos (xkon) Konstantinos <mr.xkon@gmail.com>
2017-09-13 10:40:32 +0300

    local events title update
    local events title update

--- wp-content/themes/gwtd3/local-events-page.php
+++ wp-content/themes/gwtd3/local-events-page.php
@@ -13,19 +13,20 @@ get_header(); ?>
 		<!-- Map Wrapper -->
 			<div class="row">
 				<div class="twelve columns">
-					<header class="page-header">
-						<?php
-							$page = get_post( 418 );
-							echo '<h2>' . $page->post_title . '</h2>';
-							echo $page->post_content;
-							echo '<br><br>';
-						?>
-					</header><!-- .page-header -->
-		
 					<?php
 					$query = new WP_Query( array('post_type' => 'local-event', 'posts_per_page' => -1, 'meta_key' => 'full_place', 'orderby' => 'meta_value', 'order' => 'ASC' ) );
 					$previous_continent = 'Empty';
 					$event_count = $query->post_count;
+					?>
+					<header class="page-header">
+						<h2><?php echo $event_count; ?> local events near you and counting!</h2>
+						<?php
+						$page = get_post( 418 );
+						echo $page->post_content;
+						echo '<br><br>';
+						?>
+					</header><!-- .page-header -->
+					<?php
 					// Init JS array for map markers
 					$map_datas = '<script>var markers = new Array();';
 					while ( $query->have_posts() ) : $query->the_post();
@@ -94,7 +95,6 @@ get_header(); ?>
 						</div>
 					<?php
 					endwhile;
-					echo '<br>' . $event_count . ' local events found.';
 					wp_reset_postdata();
 					// Closing map's JS data var and then write them
 					$map_datas .= '</script>';
