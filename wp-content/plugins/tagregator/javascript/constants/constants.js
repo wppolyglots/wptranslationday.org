0ee514bda4affe5103e69338f5989afcfa3c966a
Xenos Konstantinos <mr.xkon@gmail.com>
2017-10-05 20:07:53 +0300

    removing unnecessary plugins
    removing unnecessary plugins

--- wp-content/plugins/tagregator/javascript/constants/constants.js
+++
@@ -1,7 +0,0 @@
-import keyMirror from 'keymirror';
-
-// This list describes all actions that are possible in this app
-
-export default keyMirror( {
-	REQUEST_ITEMS_SUCCESS: null,
-} );
--- wp-content/plugins/tagregator/views/tggr-source-flickr/page-settings-section-header.php
+++
@@ -1,1 +0,0 @@
-<p>You can obtain the API Key by creating a Flickr account and then applying for one at <a href="https://www.flickr.com/services/">the App Garden</a>.</p>
